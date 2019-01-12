c917bac366569a527546b523abe7c324806c5e0b
Gary Lau <gary.lkw95@gmail.com>
2019-01-12 23:43:15 +1100

    Renamed convert

---
+++ R/ar_convert.R
@@ -0,0 +1,43 @@
+#' Calculate AR base on mod
+#' This function calculate new AR base on mod
+#'
+#' @param ar original AR
+#' @param enalbe_mod mod applied. Note this takes in the numeric version of mod
+#'
+#' @export
+ar_convert <- function(ar, enable_mod){
+  # Convert numeric mod to character mod
+  mod_list <- mod_detect(enable_mod)
+
+  # HR and EZ are easy to calculate as it is universal among all AR
+  if(str_detect(mod_list, "HR")){
+    ar <- min(ar * 1.4, 10)
+  }
+  if(str_detect(mod_list, "EZ")){
+    ar <- ar /2
+  }
+
+  # Convert AR to ms
+  ms <- ifelse(ar < 5,
+               1800 - ar * 120,
+               1200 - (ar-5) * 150)
+
+  # Convert HT and DT
+  if(str_detect(mod_list, "HT")){
+    ms <- ms * 4/3
+  }
+  if(str_detect(mod_list, "DT|NC")){
+    ms <- ms * 2/3
+  }
+
+  # Reverse ms to AR
+  ar <- ifelse(ms > 1200,
+               (1800 - ms) / 120,
+               (1200 - ms) / 150 + 5) %>%
+    round(2)
+
+  return(ar)
+
+}
+
+
