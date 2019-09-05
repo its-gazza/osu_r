eb0a1a8f863f101e69f0fd5a8c67c319cece05af
Gary Lau <gary.lkw95@gmail.com>
2019-09-05 23:33:28 +1000

    Added get_user test

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,8 +1,22 @@
 require(osur)
 
-test_that("Beatmap works", {
-  api <- Sys.getenv('osu_api')
-  bm_info <- get_beatmap(beatmap_id = '1944412', api_key = api)
+test_that("Beatmap api", {
+  osu_api <- Sys.getenv('osu_api')
+  bm_info <- get_beatmap(beatmap_id = 1944412, api_key = osu_api)
+
   expect_equal(bm_info$beatmap_id, '1944412')
-  expect_equal(bm_info$version, "Veteran")
+  expect_warning(get_beatmap(beatmap_id = 'abcd', osu_api))
+  expect_warning(get_beatmap('1944412', 'weird_api'))
+})
+
+test_that("User api", {
+  osu_api <- Sys.getenv('osu_api')
+  user <- 'thematrixk'
+  user_info <- get_user(user = 'thematrixk',
+                        api_key = osu_api,
+                        type = 'string')
+
+  expect_equal(tolower(user_info$username), user)
+  expect_warning(get_user('thematrixk', 123, 'string')) # Incorrect api
+  expect_warning(get_user('thematrixk', osu_api, 'id')) # Incorrect type
 })
