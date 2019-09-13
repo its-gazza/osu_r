8839c9bd1c17497a076d0b104a40db87b86d3ec4
Gary Lau <u5581323@anu.edu.au>
2019-09-13 12:46:15 +1000

    Added more checks

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,13 +1,14 @@
 require(osur)
 
 test_that("Beatmap api", {
+  # Set up API + beatmap info
   osu_api <- Sys.getenv('osu_api')
   bm_info <- get_beatmap(beatmap_id = 1944412, api_key = osu_api)
 
-  print(bm_info$beatmap_id)
   expect_equal(bm_info$beatmap_id, '1944412')
   expect_warning(get_beatmap(beatmap_id = 'abcd', osu_api))
   expect_warning(get_beatmap('1944412', 'weird_api'))
+  expect_warning(get_beatmap('194412'))
 })
 
 test_that("User api", {
@@ -18,6 +19,11 @@ test_that("User api", {
                         string = TRUE)
 
   expect_equal(tolower(user_info$username), user)
-  expect_warning(get_user('thematrixk', 123, string = TRUE)) # Incorrect api
-  expect_warning(get_user('thematrixk', osu_api, id = TRUE)) # Incorrect type
+  expect_warning(get_user('thematrix', osu_api, string = FALSE, id = FALSE)) # No identifier used
+  expect_warning(get_user('thematrix', osu_api, string = FALSE, id = FALSE)) # conflicting identifier used
+  expect_warning(get_user('thematrix', string = FALSE, id = FALSE)) # No identifier used
+  expect_warning(get_user('thematrix', id = TRUE)) # No identifier used
+  expect_warning(get_user('thematrixk', 123, string = TRUE, id = FALSE)) # Incorrect api
+  expect_warning(get_user('thematrixk', osu_api, id = TRUE, string = TRUE)) # Incorrect type
+  expect_warning(get_user('somerandomname', osu_api, id = TRUE))
 })
