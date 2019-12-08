ec9a95059a59ee7a716f8b2ae9aa8705b4da016e
Gary Lau <gary.lkw95@gmail.com>
2019-12-08 14:26:21 +1100

    Added other metric conversion

--- R/convert_mod.R
+++ R/convert_mod.R
@@ -1,8 +1,7 @@
-#' Calculate AR base on mod
-#' This function calculate new AR base on mod
+#' Convert AR
 #'
-#' @param enable_mod mod used
-#' @param ar original AR
+#' @param ar Original AR
+#' @param enable_mod mod(s) used
 #'
 #' @export
 convert_ar <- function(ar, enable_mod){
@@ -40,3 +39,125 @@ convert_ar <- function(ar, enable_mod){
 
 }
 
+
+
+#' Convert BPM
+#'
+#' @param bpm Original BMP
+#' @param enabled_mods Mod(s) used
+#'
+#' @return Converted BPM
+#' @export
+convert_bpm <- function(bpm, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+  if(stringr::str_detect(mod, "DT")){
+    bpm = bpm * 3/2
+  }
+  if(stringr::str_detect(mod, "HT")){
+    bpm = bpm * 3/4
+  }
+  return(bpm)
+}
+
+
+#' Convert drain
+#'
+#' @param diff_drain Drain difficulty
+#' @param enabled_mods Mods used
+#'
+#' @export
+
+convert_drain <- function(diff_drain, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+
+  if(stringr::str_detect(mod, "HR")){
+    diff_drain = min(diff_drain * 1.4, 10)
+  }
+  # Easy
+  if(stringr::str_detect(mod, "EZ")){
+    diff_drain = diff_drain/2
+  }
+
+  return(diff_drain)
+}
+
+# ======================================
+#' Convert hit length base on mods
+#'
+#' @param hit_length Beatmap length
+#' @param enabled_mods Mod used
+#'
+#' @export
+
+convert_hit_length <- function(hit_length, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+  if(stringr::str_detect(mod, "DT")){
+    hit_length = hit_length * 3/2
+  }
+  if(stringr::str_detect(mod, "HT")){
+    hit_length = hit_length * 3/4
+  }
+  return(hit_length)
+}
+
+
+#' Convert overall difficulrt
+#'
+#' @param diff_overall Beatmap overall difficulty
+#' @param enabled_mods Mod used
+#'
+#' @export
+convert_overall <- function(diff_overall, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+
+  if(stringr::str_detect(mod, "HR")){
+    diff_overall = min(diff_overall * 1.4, 10)
+  }
+  # Easy
+  if(stringr::str_detect(mod, "EZ")){
+    diff_overall = diff_overall/2
+  }
+
+  return(diff_overall)
+}
+
+
+#' Convert size base on mods
+#'
+#' @param diff_size Beatmap difficulty
+#' @param enabled_mods mods used
+#'
+#' @export
+#'
+convert_size <- function(diff_size, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+
+  # Hard Rock
+  if(stringr::str_detect(mod, "HR")){
+    diff_size = min(diff_size * 1.3, 10)
+  }
+  # Easy
+  if(stringr::str_detect(mod, "EZ")){
+    diff_size = diff_size/2
+  }
+  return(diff_size)
+}
+
+
+#' Convert total length base on mod
+#'
+#' @param total_length total length in beatmap
+#' @param enabled_mods mod used
+#'
+#' @export
+#'
+convert_total_length <- function(total_length, enabled_mods){
+  mod <- osur::detect_mod(enabled_mods)
+  if(stringr::str_detect(mod, "DT")){
+    total_length = total_length * 3/2
+  }
+  if(stringr::str_detect(mod, "HT")){
+    total_length = total_length * 3/4
+  }
+  return(total_length)
+}
